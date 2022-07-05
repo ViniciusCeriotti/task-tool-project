a26fbfac1feb1748edbe958d1a3949bc121ba0e4
ViniciusCeriotti <vini.ceriotti@hotmail.com>
2022-07-05 19:35:20 -0300

    First commit

---
+++ task-tool-java/src/main/java/task/tool/java/TaskToolJavaApplication.java
@@ -0,0 +1,13 @@
+package task.tool.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskToolJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskToolJavaApplication.class, args);
+	}
+
+}
---
+++ task-tool-java/src/test/java/task/tool/java/TaskToolJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package task.tool.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskToolJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
